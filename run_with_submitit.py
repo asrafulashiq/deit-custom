ee4244afb8662e75e7e156e65a8a6610de942503
asrafulashiq <asrafulashiq@gmail.com>
2021-01-19 18:25:30 -0500

    update

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -50,8 +50,8 @@ def parse_args():
 
 def get_shared_folder() -> Path:
     user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    if Path("checkpoint/").is_dir():
+        p = Path(f"checkpoint/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
